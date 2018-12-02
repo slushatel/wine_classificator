d13fecc4e442f3e0ef6b8fe71e31cd6eab665b3c
stas <q>
2018-12-02 21:16:32 +0200

    add code

--- main.py
+++ main.py
@@ -57,4 +57,21 @@ X_train, X_test, y_train, y_test = train.Trainer().split_to_train_test_sets(wine
 
 X_train, X_test = train.Trainer().standartize_data(X_train, X_test)
 
+# Import `Sequential` from `keras.models`
+from keras.models import Sequential
+
+# Import `Dense` from `keras.layers`
+from keras.layers import Dense
+
+# Initialize the constructor
+model = Sequential()
+
+# Add an input layer
+model.add(Dense(12, activation='relu', input_shape=(11,)))
+
+# Add one hidden layer
+model.add(Dense(8, activation='relu'))
+
+# Add an output layer
+model.add(Dense(1, activation='sigmoid'))
 
